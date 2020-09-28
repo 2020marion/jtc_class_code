88d19ada4c8a20896a7b319685a66d91b393d281
2020marion <maryonrodriguez11@gmail.com>
2020-09-28 17:52:18 -0400

    nba_challenge.py

---
+++ challenges/04_primitive_data_types_part_2/nba_challenge.py
@@ -0,0 +1,28 @@
+#2.1 create  variables for number of 3 point shots made by players
+jm3_made = 46
+fv3_made = 43
+jh3_made = 37
+#2.2 print out the number of 3point shots using f strings with the variables created
+#print(f"In the 2020 NBA Playoffs Jamal Murray made {jm3_made} three point shots")
+#print(f"In the 2020 NBA Playoffs Fred VanVleet made {fv3_made} three point shots")
+#print(f"In the 2020 NBA Playoffs James Harden made {jh3_made} three point shots")
+#2.3 create variables of 3 point shot attempts for each player
+jm3_att = 93
+fv3_att = 110
+jh3_att = 109
+#2.4 copy print statements from 2.2 and print  both 3 made and 3 attempted
+#print(f"In the 2020 NBA Playoffs Jamal Murray made {jm3_made} three point shots and attempted {jm3_att}")
+#print(f"In the 2020 NBA Playoffs Fred VanVleet made {fv3_made} three point shots and attempted {fv3_att}")
+#print(f"In the 2020 NBA Playoffs James Harden made {jh3_made} three point shots and attempted {jh3_att}")
+#2.5 calculate and print the 3 pt percentage
+jm_percent = 46/93
+fv_percent = 43/110
+jh_percent = 37/109
+#print(f"The 2020 three point percentage for Jamal Murray was {jm_percent}")
+#print(f"The 2020 three point percentage for Fred VanVleet was {fv_percent}")
+#print(f"The 2020 three point percentage for James Harden was {jh_percent}")
+#3.1 formatting string info type paragraph with only 1 sentence per line
+#print("The Lakers went all in this offseason and swung a deal for former Pelicans forward Anthony Davis.  They sent a package of Brandon Ingram, Lonzo\Ball, Josh Hart, and 3 first-round picks to New Orleans to land Davis.  Those three have made good developments with the Pelicans, especially\Brandon Ingram.  But, the deal is still a huge win for the Lakers as Lebron, Davis, and company have put together an incredible season.  Los Angeles\has ridden James and Davis, along with a supporting cast built around them, to the second-best record in the NBA.  The Lakers ended the season\ atop the Western Conference with a record of 49-14.  They were narrowly behind the Bucks for the best record in the league.  Davis proved to be  the \ final piece necessary for the Lakers to rebound from missing the playoffs last year.  Los Angeles was a dominant club on both sides of the ball and\ are in a position to have another successful year next season.")
+#3.4 make a boolean variable indidating whether the stmt is true
+#lakers_are_best = 1
+#print("The Lakers are the best bastketball team in the NBA {lakers_are_best}")
